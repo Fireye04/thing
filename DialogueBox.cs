3ae4f774f1bafdb62dd69848f697ca9df3bc303e
Fireye <codekai16@gmail.com>
2023-12-27 22:18:38 -0700

    fixes

--- DialogueBox.cs
+++ DialogueBox.cs
@@ -6,15 +6,28 @@ public partial class DialogueBox : Node2D
 {
 
 	ArrayList ChoiceButtons = new ArrayList();
-	
-	
+	 
+	private string Prompt;
+	private VBoxContainer Choices;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		Prompt = GetNode<HBoxContainer>("HBoxContainer").GetNode<Label>("Prompt").Text;
+		Choices = GetNode<HBoxContainer>("HBoxContainer").GetNode<VBoxContainer>("VBoxContainer");
 		add_text("gay sex");
 		add_choice("why0");
-		add_choice("bc i said so 1");
+		add_choice("bc i said so1");
+	}
+
+	public void ClearDialogueBox() {
+		Prompt = "";
+		for (int i = 0; i < ChoiceButtons.Count; i++) {
+			ChoiceButton item = (ChoiceButton) ChoiceButtons[i];
+			Choices.RemoveChild(item);
+		}
+		
+		ChoiceButtons.Clear();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -23,7 +36,7 @@ public partial class DialogueBox : Node2D
 	}
 	
 	public void add_text(string text) {
-		GetNode<HBoxContainer>("HBoxContainer").GetNode<Label>("Prompt").Text = text;
+		Prompt = text;
 	}
 	
 	public void add_choice(string choice_text) {
@@ -33,7 +46,7 @@ public partial class DialogueBox : Node2D
 		ChoiceButtons.Add(CButton);
 		CButton.Text = choice_text;
 		CButton.ChoiceSelected += OnChoiceSelected;
-		GetNode<HBoxContainer>("HBoxContainer").GetNode<VBoxContainer>("VBoxContainer").AddChild(CButton);
+		Choices.AddChild(CButton);
 
 	}
 
